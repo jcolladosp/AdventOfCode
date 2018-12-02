d218a19972a76a0817cbc453d5a79b0cbb964d1e
Jose Collado San Pedro <jocolsan@inf.upv.es>
2018-12-02 10:41:11 +0100

    Day 2 part 1

---
+++ src/Day02/Day02.kt
@@ -0,0 +1,24 @@
+package Day02
+
+import java.io.File
+
+private val ids = File("src/Day02/input.txt").readLines().map { it }
+var twoTimes = 0
+var threTimes = 0
+
+fun part1(): Int{
+    ids.forEach { word ->
+        var repeatedCharacters = word.split("").groupingBy { it }.eachCount().filterValues { it > 1 }.filterKeys { it != "" }
+        if (repeatedCharacters.filterValues { it == 2 }.isNotEmpty()) twoTimes++
+        if (repeatedCharacters.filterValues { it == 3 }.isNotEmpty()) threTimes++
+
+    }
+    return twoTimes * threTimes
+}
+
+fun main(args : Array<String>) {
+
+    println("Part 1: Checksum: ${part1()}")
+    //println("Part 2: Repeated frequency: ${part2()}")
+
+}
